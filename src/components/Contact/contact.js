536d474df92eb9ddd9e12d9a72d1af8b0999af78
Wesley PC <wesley.tejeda95@gmail.com>
2020-08-08 21:38:24 -0400

    Updated Contact component

--- src/components/Contact/contact.js
+++ src/components/Contact/contact.js
@@ -3,8 +3,33 @@ import styles from "./style.css";
 
 export default function Contact(){
     return (
-        <div>
-            <h1>Contact Page</h1>
+        <div className="container pt-4 pb-3">
+            <section className="row">
+                <section className="col-lg-7 col-md-12 col-sm-12">
+                    <div className="card">
+                        <div className="card-title border-bottom ml-3 mb-0">
+                            <h2 className="mt-3 ml textLightBlue">Contact</h2>
+                        </div>
+                        <div className="card-body mb-5 pb-4">
+                            <form>
+                                <div className="form-group">
+                                    <label for="inputName">Name</label>
+                                    <input className="form-control" id="inputName" placeholder="John Smith" />
+                                </div>
+                                <div className="form-group">
+                                    <label for="inputEmail">Email</label>
+                                    <input className="form-control" id="inputEmail" placeholder="example@mail.com" />
+                                </div>
+                                <div className="form-group">
+                                    <label for="inputMessage">Message</label>
+                                    <input className="form-control" id="inputMessage" style={{height: "100px"}} />
+                                </div>
+                                <button type="submit" className="btn btn-primary">Submit</button>
+                            </form>
+                        </div>
+                    </div>
+                </section>
+            </section>
         </div>
     )
 }
