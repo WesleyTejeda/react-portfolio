2f75c7bbd2d52eefb153c8e5495f65c161f984fa
Wesley PC <wesley.tejeda95@gmail.com>
2020-08-08 21:27:58 -0400

    Defined router and imported components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
 import React from 'react';
 import logo from './logo.svg';
+import { BrowserRouter as Router, Route}  from "react-router-dom";
+import NavTab from "./components/NavTab";
+import About from "./components/About";
+import Portfolio from "./components/Portfolio";
+import Contact from "./components/Contact";
+import Footer from "./components/Footer";
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <NavTab />
+        <Route exact path="/" component={About} />
+        <Route exact path="/portfolio" component={Portfolio} />
+        <Route exact path="/contact" component={Contact} />
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
