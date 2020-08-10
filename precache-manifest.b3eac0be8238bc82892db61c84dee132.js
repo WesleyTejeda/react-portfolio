20930bf5a8d92282ac8dccf8d48d33af6d31f3a4
Wesley PC <wesley.tejeda95@gmail.com>
2020-08-09 22:25:31 -0400

    Updates

---
+++ precache-manifest.b3eac0be8238bc82892db61c84dee132.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c4a6a00189092ced09f4aa4b1cd67802",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7afba2cb1f82452d7449",
+    "url": "/static/css/main.2b56716e.chunk.css"
+  },
+  {
+    "revision": "be64371eee7ba68e8b15",
+    "url": "/static/js/2.488df8e9.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.488df8e9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7afba2cb1f82452d7449",
+    "url": "/static/js/main.7c514d3b.chunk.js"
+  },
+  {
+    "revision": "dd8147bbe7734801bdde",
+    "url": "/static/js/runtime-main.e312f208.js"
+  },
+  {
+    "revision": "35d3c49a72f965bc9cefcfc48ae68a37",
+    "url": "/static/media/bg3.35d3c49a.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
