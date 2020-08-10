a2b6e6e9a6171eeb94f4f2d9cb4a359c3830b7be
Wesley PC <wesley.tejeda95@gmail.com>
2020-08-09 22:24:17 -0400

    Updated style on text

--- src/components/About/about.js
+++ src/components/About/about.js
@@ -12,7 +12,7 @@ export default function About() {
                     <div className="col-lg-6 col-md-6 col-sm-12 card-body">
                         <img className="aboutPic" src="images/myPic.png" alt="Wesley Tejeda, thumbs up!" />
                     </div>
-                    <article className="pb-4 mb-2">
+                    <article className="pb-4 mb-2" style={{fontSize: "18px"}}>
                         Welcome, I'm Wesley, or Wes for short. I'm a full-stack web developer currently residing in Philadelphia, PA. I'm a confident, skillful, and dedicated individual with a love for technology and programming. Along with using present day programming technologies I possess great knowledge in regards to software development, backend server configuration, object-oriented programming and I'm highly adept in both solo and team-based environments. Now that you know more about me you can navigate to the other webpages on this site, such as the portfolio section which showcases a few of my web applications. Safe travels.
                             </article>
                     <p className="pt-1">Email: Wesley.Tejeda95@gmail.com</p>
