22a6836f0649d6b380bfda309fa08281590454d8
Wesley PC <wesley.tejeda95@gmail.com>
2020-08-09 22:35:47 -0400

    Updates

---
+++ precache-manifest.5640cf2738b9aa12a1fd6960e53d5ee1.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "afb298a4e86d2866fba500f24ccfbccd",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "aa1b45e8a09a8f7838ba",
+    "url": "/react-portfolio/static/css/main.fa38e79b.chunk.css"
+  },
+  {
+    "revision": "2d0502e12911790bf729",
+    "url": "/react-portfolio/static/js/2.82c212fc.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-portfolio/static/js/2.82c212fc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aa1b45e8a09a8f7838ba",
+    "url": "/react-portfolio/static/js/main.d8ea9cea.chunk.js"
+  },
+  {
+    "revision": "aff33cef9dbb8787d6b3",
+    "url": "/react-portfolio/static/js/runtime-main.213b1880.js"
+  },
+  {
+    "revision": "35d3c49a72f965bc9cefcfc48ae68a37",
+    "url": "/react-portfolio/static/media/bg3.35d3c49a.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-portfolio/static/media/logo.5d5d9eef.svg"
+  }
+]);
