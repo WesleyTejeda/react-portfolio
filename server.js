bc99e33b4d1f9fc15447eca3f66d6f470b3f966d
Wesley PC <wesley.tejeda95@gmail.com>
2020-08-11 00:09:54 -0400

    Updated server

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use(express.json());
 app.use(express.static("client/public"));
 
 app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname + "../client/build/index.html"));
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 // Add routes, both API and view
 app.use(routes);
