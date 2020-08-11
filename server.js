0f9d98ad786bb4e4c520c6aa16ff66d31b7a779a
Wesley PC <wesley.tejeda95@gmail.com>
2020-08-11 00:26:45 -0400

    Updated server

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const PORT = process.env.PORT || 3001;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 // Serve up static assets (usually on heroku)
-app.use(express.static("client/public"));
+app.use(express.static("client/build"));
 
 
 // Add routes, both API and view
