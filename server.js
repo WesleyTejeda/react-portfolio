859aa7597c9459cad92c66f82b99d22872631ab1
Wesley PC <wesley.tejeda95@gmail.com>
2020-08-11 00:21:24 -0400

    Updated server

--- server.js
+++ server.js
@@ -9,11 +9,14 @@ app.use(express.json());
 // Serve up static assets (usually on heroku)
 app.use(express.static("client/public"));
 
+
+// Add routes, both API and view
+app.use(routes);
+
 app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
 });
-// Add routes, both API and view
-app.use(routes);
+
 // Start the API server
 app.listen(PORT, function() {
   console.log(`Server now listening on PORT ${PORT}!`);
